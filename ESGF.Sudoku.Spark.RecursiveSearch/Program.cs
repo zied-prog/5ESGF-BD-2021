c7ad3023aa035884946f1a7efcd3ed9f2842f7f8
Jean-Sylvain Boige <jsboige@gmail.com>
2021-02-24 12:59:27 +0100

    Ajout de la solution et exemple d'application console avec package NuGet

---
+++ ESGF.Sudoku.Spark.RecursiveSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ESGF.Sudoku.Spark.RecursiveSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
